e8feb5ff909953bffbef4f630515ffbb849176a5
Папенов Роман <roman.papenov@myway.local>
2021-03-07 01:32:18 +0300

    roundabout slider done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,9 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+
 
         <!-- Styles -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
--- resources/views/menu/main.blade.php
+++ resources/views/menu/main.blade.php
@@ -18,11 +18,19 @@
         <div id="featured">
             <ul>
                 <li>
-                    <h1>Добро пожаловать на сайт товарного направления "Посуда и Текстиль"</h1>
-                    <h2>Мы рады рассказать Вам о наших новинках на сезон 2021 - 2022</h2>
+                    <div class="hero-container">
+                        <h1>"Посуда и Домашний текстиль"</h1>
+                        <h2>Новинки сезона 2021 - 2022</h2>
+                        <a href="{{ route('nav::catalog') }}" class="btn-get-started scrollto">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-chevron-double-down" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                                <path fill-rule="evenodd" d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                            </svg>
+                        </a>
+                    </div>
                     <a href="{{ route('nav::goods', ['id' => 4]) }}">
-                        <span>Керамика</span>
-                        <img src="storage/img/good/824-443.jpg" alt="" />
+                        <span>Текстиль</span>
+                        <img src="storage/img/good/434-081.jpg" alt="" />
                     </a>
                 </li>
                 <li>
@@ -33,8 +41,8 @@
                 </li>
                 <li>
                     <a href="{{ route('nav::goods', ['id' => 2]) }}">
-                        <span>Текстиль</span>
-                        <img src="storage/img/good/434-081.jpg" alt="" />
+                        <span>Керамика</span>
+                        <img src="storage/img/good/824-443.jpg" alt="" />
                     </a>
                 </li>
                 <li>
