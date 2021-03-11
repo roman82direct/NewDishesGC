5e99519bd511a205746ee7589efb423cba698321
Папенов Роман <roman.papenov@myway.local>
2021-03-11 20:10:29 +0300

    add pagination admin Goods

--- resources/views/admin/goods.blade.php
+++ resources/views/admin/goods.blade.php
@@ -19,7 +19,23 @@
         @endif
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                @dump($goods)
+                <div class="grid-4">
+                    @forelse($goods as $item)
+                        <div class="card" style="width: 17rem;">
+                            <img style="" src="../{{ $item->img }}" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h4 class="card-title">{{ $item->art }}</h4>
+                                <h5 class="card-title">{{ $item->name }}</h5>
+                                <p class="card-text">{{ (\App\Models\Category::find($item->category_id)->name) }}</p>
+                                <a href="#" class="btn btn-primary">{{ __('buttons.update') }}</a>
+                                <a href="#" class="btn btn-danger">{{ __('buttons.delete') }}</a>
+                            </div>
+                        </div>
+                    @empty
+                        Товаров нет!
+                    @endforelse
+                </div>
+            {{ $goods->links('vendor.pagination.tailwind') }}
             </div>
         </div>
     </div>
