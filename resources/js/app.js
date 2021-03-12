6a412a5afec5817f2341b04f771db7482340c5fd
Папенов Роман <roman.papenov@myway.local>
2021-03-13 00:35:50 +0300

    add swiper pagination

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,4 +2,30 @@ require('./bootstrap');
 
 require('alpinejs');
 
+import Swiper from 'swiper/bundle';
 
+$(document).ready(function (){
+    /**
+     * Portfolio details slider
+     */
+    new Swiper('.portfolio-details-slider', {
+        speed: 400,
+        loop: true,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        }
+    });
+});
--- resources/views/goodItem.blade.php
+++ resources/views/goodItem.blade.php
@@ -13,127 +13,59 @@
         </h2>
     </x-slot>
 
-    <!-- ======= Portfolio Details Section ======= -->
-    <section id="portfolio-details" class="portfolio-details">
+    <div class="py-8">
+    <section id="portfolio-details" class="portfolio-details bg-white shadow-sm max-w-7xl mx-auto sm:rounded-lg">
         <div class="container">
-
             <div class="row gy-4">
-
                 <div class="col-lg-8">
                     <div class="portfolio-details-slider swiper-container">
                         <div class="swiper-wrapper align-items-center">
-
                             <div class="swiper-slide">
-                                <img src="assets/img/portfolio/portfolio-1.jpg" alt="">
+                                <img src="{{ $item->img }}" alt="">
                             </div>
-
                             <div class="swiper-slide">
-                                <img src="assets/img/portfolio/portfolio-2.jpg" alt="">
+                                <img src="{{ $item->img1 }}" alt="">
                             </div>
-
                             <div class="swiper-slide">
-                                <img src="assets/img/portfolio/portfolio-3.jpg" alt="">
+                                <img src="{{ $item->img2 }}" alt="">
                             </div>
-
                         </div>
                         <div class="swiper-pagination"></div>
+                        <div class="swiper-button-prev"></div>
+                        <div class="swiper-button-next"></div>
+                        <!-- If we need scrollbar -->
+                        {{--                        <div class="swiper-scrollbar"></div>--}}
                     </div>
                 </div>
 
                 <div class="col-lg-4">
                     <div class="portfolio-info">
-                        <h3>Project information</h3>
+                        <h3>Информация</h3>
                         <ul>
-                            <li><strong>Category</strong>: Web design</li>
-                            <li><strong>Client</strong>: ASU Company</li>
-                            <li><strong>Project date</strong>: 01 March, 2020</li>
-                            <li><strong>Project URL</strong>: <a href="#">www.example.com</a></li>
+                            <li><strong>Артикул</strong>: {{ $item->art }}</li>
+                            <li><strong>Наименование</strong>: {{ $item->name }}</li>
+                            <li><strong>Категория</strong>: {{ \App\Models\Category::find($item->category_id)->name}}</li>
+                            <li><strong>Дата прихода</strong>: {{ $item->arrival }}</li>
+                            <li><strong>Упаковка</strong>: <a href="#">www.example.com</a></li>
                         </ul>
                     </div>
                     <div class="portfolio-description">
-                        <h2>This is an example of portfolio detail</h2>
-                        <p>
-                            Autem ipsum nam porro corporis rerum. Quis eos dolorem eos itaque inventore commodi labore quia quia. Exercitationem repudiandae officiis neque suscipit non officia eaque itaque enim. Voluptatem officia accusantium nesciunt est omnis tempora consectetur dignissimos. Sequi nulla at esse enim cum deserunt eius.
-                        </p>
+                        <h2>Описание</h2>
+                        <p>{{ $item->description }}</p>
                     </div>
                 </div>
+                @auth()
+                    @if(Auth::user()->hasRole('admin'))
+                        <div class="md:inline-flex mypd">
+                            <a class="btn btn-outline-secondary mymrgleft" href="{{route('admin::good::update', ['id' => $item->id])}}">{{ __('buttons.update') }}</a>
+                            <a class="btn btn-outline-danger mymrgleft" href="{{route('admin::good::delete', ['id' => $item->id])}}">{{ __('buttons.delete') }}</a>
+                        </div>
+                    @endif
+                @endauth
 
             </div>
 
         </div>
-    </section><!-- End Portfolio Details Section -->
-
-    <div class="py-8">
-        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg goodCard">
-
-                <div class="row gy-4">
-
-                    <div class="col-lg-8">
-                        <div id="carouselExampleIndicators_{{ $item->id }}" class="carousel slide" data-bs-ride="carousel">
-                            <div class="carousel-indicators">
-                                <button type="button" data-bs-target="#carouselExampleIndicators_{{ $item->id }}" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators_{{ $item->id }}" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                                <button type="button" data-bs-target="#carouselExampleIndicators_{{ $item->id }}" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                            </div>
-                            <div class="carousel-inner">
-                                <div class="carousel-item active vh70">
-                                    <img src="{{ $item->img }}" class="d-block w-100" alt="...">
-                                </div>
-                                <div class="carousel-item vh70">
-                                    <img src="{{ $item->img1 }}" class="d-block w-100" alt="...">
-                                </div>
-                                <div class="carousel-item vh70">
-                                    <img src="{{ $item->img2 }}" class="d-block w-100" alt="...">
-                                </div>
-                            </div>
-                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators_{{ $item->id }}"  data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
-                            </button>
-                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators_{{ $item->id }}"  data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
-                            </button>
-                        </div>
-
-                    <div class="col-lg-4">
-                        <div class="portfolio-info">
-                            <h3>Project information</h3>
-                            <ul>
-                                <li><strong>Артикул</strong>: {{ $item->art }}</li>
-                                <li><strong>Наименование</strong>: <a href="#">www.example.com</a></li>
-                                <li><strong>Категория</strong>: Web design</li>
-                                <li><strong>Коллекция</strong>: 01 March, 2020</li>
-                                <li><strong>Упаковка</strong>: ________</li>
-                                <li><strong>Дата прихода</strong>: 01 March, 2020</li>
-                            </ul>
-                        </div>
-                        <div class="portfolio-description">
-                            <h2>Описание</h2>
-                            <p></p>
-                        </div>
-                    </div>
-                        @auth()
-                            @if(Auth::user()->hasRole('admin'))
-                                <div class="md:inline-flex mypd">
-                                    <a class="btn btn-outline-secondary mymrgleft" href="{{route('admin::good::update', ['id' => $item->id])}}">{{ __('buttons.update') }}</a>
-                                    <a class="btn btn-outline-danger mymrgleft" href="{{route('admin::good::delete', ['id' => $item->id])}}">{{ __('buttons.delete') }}</a>
-                                </div>
-                            @endif
-                        @endauth
-
-                </div>
-
-
-
-{{--                <div class="goodItem">--}}
-{{--                    <h2 class="itemHeader">{{ $item->art }}</h2>--}}
-
-{{--                    <p class="itemDescription">{{ $item->name }}</p>--}}
-
-                </div>
-            </div>
-        </div>
+    </section>
     </div>
 </x-app-layout>
