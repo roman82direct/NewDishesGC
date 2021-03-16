d479ef3a4e914f52ed046a165a0836b5a1414f52
Папенов Роман <roman.papenov@myway.local>
2021-03-16 17:19:09 +0300

    add footer

--- resources/views/menu/main.blade.php
+++ resources/views/menu/main.blade.php
@@ -64,7 +64,7 @@
             <div class="row">
                 <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
                     <div class="icon-box" data-aos="fade-up">
-                        <div class="icon"><i class="bx bxl-dribbble"></i></div>
+                        <div class="icon"><i class="bi-alarm"></i></div>
                         <h4 class="title"><a href="">Lorem Ipsum</a></h4>
                         <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
                     </div>
@@ -72,7 +72,7 @@
 
                 <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
                     <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
-                        <div class="icon"><i class="bx bx-file"></i></div>
+                        <div class="icon"><i class="bi bi-archive"></i></div>
                         <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
                         <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
                     </div>
@@ -80,7 +80,7 @@
 
                 <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
                     <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
-                        <div class="icon"><i class="bx bx-tachometer"></i></div>
+                        <div class="icon"><i class="bi bi-bag-check"></i></div>
                         <h4 class="title"><a href="">Magni Dolores</a></h4>
                         <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
                     </div>
@@ -88,7 +88,7 @@
 
                 <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
                     <div class="icon-box" data-aos="fade-up" data-aos-delay="300">
-                        <div class="icon"><i class="bx bx-world"></i></div>
+                        <div class="icon"><i class="bi bi-basket"></i></div>
                         <h4 class="title"><a href="">Nemo Enim</a></h4>
                         <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
                     </div>
@@ -113,6 +113,5 @@
             </div>
         </section><!-- End Cta Section -->
     </div>
-
 
    </x-app-layout>
