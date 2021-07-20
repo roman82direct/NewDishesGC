a7bcbe7ee606c6523e7905a00c14a671e1eab0e9
Roman <roman82direct@yandex.ru>
2021-07-20 13:11:26 +0300

    add components.offcanvas.comment on goodItem page

--- resources/views/goodItem.blade.php
+++ resources/views/goodItem.blade.php
@@ -52,7 +52,7 @@
                             @endif
                             <li><strong>Упаковка</strong>: {{ $item->pack }}
                                 @if($item->img_pack)
-                                    <a href="" data-bs-toggle="modal" data-bs-target="#packImgModal">
+                                    <a class="pack-link" href="" data-bs-toggle="modal" data-bs-target="#packImgModal">
                                         <img style="height: 100px" src="{{ $item->img_pack }}" alt="">
                                     </a>
                                 @endif
@@ -113,7 +113,7 @@
             <div class="section-title" data-aos="fade-in" data-aos-delay="100">
                 <h2>Отзывы</h2>
             </div>
-            @php($comments = \App\Models\Comment::whereGoodId($item->id)->get())
+            @php($comments = \App\Models\Comment::whereGoodId($item->id)->where('is_moderate', 1)->get())
             @if($comments->count() == 0)
                 <h3>Отзывов пока нет.</h3>
             @else
@@ -140,31 +140,6 @@
         </div>
     </section><!-- End Testimonials Section -->
 
-    <div class="offcanvas offcanvas-bottom" tabindex="-1" id="offcanvas" aria-labelledby="offcanvasLabel">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasLabel">{{ Auth::user() ? 'Оставьте свой комментарий в текстовом поле ниже...' : 'Авторизуйтесь, чтобы оставить комментарий...' }}</h5>
-            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-            @auth()
-            <div>
-                <form id="commentForm">
-                    @csrf
-                    <input id="goodId" name="goodId" type="hidden" value="{{ $item->id }}">
-                    <input id="userId" name="userId" type="hidden" value="{{ \Illuminate\Support\Facades\Auth::id() }}">
-                    <div class="mb-3">
-                        <textarea class="form-control" id="comment" name="comment" rows="4"></textarea>
-                    </div>
-                </form>
-                <button type="button" id="toCommentBtn" data-bs-dismiss="offcanvas"
-                        class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent
-                               rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700
-                               active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25
-                               transition ease-in-out duration-150">Отправить
-                </button>
-            </div>
-            @endauth
-        </div>
-    </div>
+    @include('components.offcanvas.comment-good')
     @include('components.modals.showGoodPackImg')
 </x-app-layout>
