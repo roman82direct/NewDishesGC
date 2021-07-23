938fc4a47aab04e14ef12bebc268287e8356e38f
Roman <roman82direct@yandex.ru>
2021-07-23 22:30:14 +0300

    add adaptive search&favorites styles

--- resources/views/layouts/searchresult.blade.php
+++ resources/views/layouts/searchresult.blade.php
@@ -1,7 +1,7 @@
 @if(count($searchgoods) > 0)
     @foreach($searchgoods as $item)
-        <li class=""><a href="{{ route('nav::showGoodItem', ['id'=>$item->id]) }}">{{ $item->art }}  {{ mb_strimwidth($item->name, 0, 50, '...') }}</a></li>
+        <li><a href="{{ route('nav::showGoodItem', ['id'=>$item->id]) }}">{{ $item->art }}  {{ mb_strimwidth($item->name, 0, 50, '...') }}</a></li>
     @endforeach
 @else
-    <li>Ничего не найдено</li>
+    <li><i>Ничего не найдено...</i></li>
 @endif
