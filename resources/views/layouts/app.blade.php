15a5ff31fc0db7283115127307dc57edc2ea7b88
Roman <roman82direct@yandex.ru>
2021-07-24 14:11:18 +0300

    add favicon icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-{{--        <link rel="shortcut icon" href="{{ asset('storage/img/favicon.png') }}">--}}
+        <link rel="shortcut icon" href="{{ asset('storage/img/favicon.png') }}" >
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
