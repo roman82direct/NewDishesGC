cb0a57910db9d5bef840822963118313314fb4b2
Roman <roman82direct@yandex.ru>
2021-10-08 14:29:38 +0300

    remove orchestra/parser

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Orchestra\Parser\XmlServiceProvider::class,
+//        Orchestra\Parser\XmlServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -228,7 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'FastExcel' => Rap2hpoutre\FastExcel\Facades\FastExcel::class, //excel import/export package
-        'XmlParser' => Orchestra\Parser\Xml\Facade::class,
+//        'XmlParser' => Orchestra\Parser\Xml\Facade::class,
     ],
 
 ];
